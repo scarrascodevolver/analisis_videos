5e9832cd61b89942319c0df7a46774e4a3537d4e
scarrascodevolver <scarrascodevolver@gmail.com>
2025-08-28 10:40:56 -0400

    Update TeamSeeder with all rugby teams including Los Troncos, DOBS, All Brads A, etc.

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -13,20 +13,23 @@ class TeamSeeder extends Seeder
     public function run(): void
     {
         $teams = [
-            ['name' => 'LOS TRONCOS', 'abbreviation' => 'LT', 'is_own_team' => true],
+            ['name' => 'Los Troncos', 'abbreviation' => 'LT', 'is_own_team' => true],
             ['name' => 'DOBS', 'abbreviation' => 'DOBS', 'is_own_team' => false],
-            ['name' => 'ALL BRADS', 'abbreviation' => 'AB', 'is_own_team' => false],
-            ['name' => 'OLD GEORGIANS', 'abbreviation' => 'OG', 'is_own_team' => false],
-            ['name' => 'TABANCURA RC', 'abbreviation' => 'TRC', 'is_own_team' => false],
-            ['name' => 'OLD GABS', 'abbreviation' => 'OGB', 'is_own_team' => false],
-            ['name' => 'LAGARTOS RC', 'abbreviation' => 'LRC', 'is_own_team' => false],
-            ['name' => 'OLD ANGLONIANS', 'abbreviation' => 'OA', 'is_own_team' => false],
-            ['name' => 'OLD LOCKS', 'abbreviation' => 'OL', 'is_own_team' => false],
-            ['name' => 'COSTA DEL SOL', 'abbreviation' => 'CDS', 'is_own_team' => false],
+            ['name' => 'All Brads A', 'abbreviation' => 'ABA', 'is_own_team' => false],
+            ['name' => 'Old Georgians', 'abbreviation' => 'OG', 'is_own_team' => false],
+            ['name' => 'Tabancura RC', 'abbreviation' => 'TRC', 'is_own_team' => false],
+            ['name' => 'Old Locks', 'abbreviation' => 'OL', 'is_own_team' => false],
+            ['name' => 'Lagartos RC', 'abbreviation' => 'LRC', 'is_own_team' => false],
+            ['name' => 'Old Gabs', 'abbreviation' => 'OGB', 'is_own_team' => false],
+            ['name' => 'Old Anglonians', 'abbreviation' => 'OA', 'is_own_team' => false],
+            ['name' => 'Costa del Sol', 'abbreviation' => 'CDS', 'is_own_team' => false],
         ];
 
         foreach ($teams as $team) {
-            \App\Models\Team::create($team);
+            \App\Models\Team::firstOrCreate(
+                ['name' => $team['name']],
+                $team
+            );
         }
     }
 }
